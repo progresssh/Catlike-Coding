ebc415759fba046173620444e8fa2f67d5d2884e
progress <star@progress.sh>
2024-03-14 17:47:54 +0100

    Added Update method to Graph

--- Basics/Graph/Assets/Script/Graph.cs
+++ Basics/Graph/Assets/Script/Graph.cs
@@ -1,3 +1,6 @@
+
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Graph : MonoBehaviour
@@ -7,20 +10,34 @@ public class Graph : MonoBehaviour
 
   [SerializeField, Range(10, 100)]
   int resolution = 10;
+  Transform[] points;
   void Awake()
   {
+
     float step = 2f / resolution;
     Vector3 position = Vector3.zero;
     var scale = Vector3.one * step;
 
-    for (int i = 0; i < resolution; i++)
+    points = new Transform[resolution];
+
+    for (int i = 0; i < points.Length; i++)
     {
-      Transform point = Instantiate(pointPrefab);
+      Transform point = points[i] = Instantiate(pointPrefab);
       position.x = (i + 0.5f) * step - 1f;
-      position.y = position.x * position.x * position.x;
       point.localPosition = position;
       point.localScale = scale;
       point.SetParent(transform, false);
+    }
+  }
+
+  void Update()
+  {
+    for (int i = 0; i < points.Length; i++)
+    {
+      Transform point = points[i];
+      Vector3 position = point.localPosition;
+      position.y = Mathf.Sin(Mathf.PI * position.x);
+      point.localPosition = position;
     }
   }
 }
