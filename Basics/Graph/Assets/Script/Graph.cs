735a33c22c93a2d0260c4692c52d33b214d42b0d
progress <star@progress.sh>
2024-03-15 23:30:18 +0100

    animated sine + clamped colors GRAPH FINISHED

--- Basics/Graph/Assets/Script/Graph.cs
+++ Basics/Graph/Assets/Script/Graph.cs
@@ -32,11 +32,12 @@ public class Graph : MonoBehaviour
 
   void Update()
   {
+    float time = Time.time;
     for (int i = 0; i < points.Length; i++)
     {
       Transform point = points[i];
       Vector3 position = point.localPosition;
-      position.y = Mathf.Sin(Mathf.PI * position.x);
+      position.y = Mathf.Sin(Mathf.PI * (position.x + time));
       point.localPosition = position;
     }
   }
